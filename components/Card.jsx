4a6af818347226b24850aba5f9698f55af7edd78
MDaud32 <theprofessor543@gmail.com>
2023-03-03 22:36:32 +0500

    fixing card

--- components/Card.jsx
+++ components/Card.jsx
@@ -15,151 +15,143 @@ import { useTheme } from "@mui/material/styles";
 
 const CardPage = () => {
   const theme = useTheme();
+  console.log(theme.breakpoints.up("ipadLandscape"));
 
   const isIpadLandscape = useMediaQuery(theme.breakpoints.up("ipadLandscape"));
+  console.log(isIpadLandscape);
 
   return (
-    <div>
-      {isIpadLandscape ? (
-        <p>iPad landscape mode</p>
-      ) : (
-        <p>Not iPad landscape mode</p>
-      )}
-    </div>
+    <Box sx={{ bgcolor: { md: "red", ipadlandscape: "green" } }}>
+      {data.map((card) => (
+        <Box
+          key={card.id}
+          sx={{
+            maxWidth: { xs: "100%", sm: "100%", md: "100%" },
+            display: "flex",
+            flexDirection: { xs: "column", sm: "row" },
+            mb: 2,
+            height: { md: 200 },
+            backgroundColor: "#ffffff",
+            boxShadow: "1px 1px 1px 1px #eaeaea",
+          }}>
+          <Carousel showIndicators={false} showThumbs={false}>
+            {items.map((itemImg) => (
+              // eslint-disable-next-line react/jsx-key
+              <Box
+                component="img"
+                sx={{
+                  height: 200,
+                  display: "block",
+                  maxWidth: { xs: "100%", sm: "400" },
+                  objectFit: "cover",
+                  overflow: "hidden",
+                  width: "100%",
+                }}
+                src={itemImg.image}
+                key={itemImg.id}
+                alt={"haha"}
+              />
+            ))}
+          </Carousel>
+          <Link href="/DetailPage" underline="none" key={card.id}>
+            <CardActionArea
+              sx={{
+                flexDirection: { xs: "column", sm: "row" },
+              }}>
+              <CardContent>
+                <Box width={{ xs: "100%", sm: 500, md: "100%" }}>
+                  <Stack direction="row" justifyContent="space-between">
+                    {/* rent and location */}
+                    <Stack
+                      direction="row"
+                      width={{ xs: "100%", md: "50%" }}
+                      justifyContent="space-between"
+                      alignItems="center">
+                      <Typography
+                        sx={{
+                          color: "#6F8CD3",
+                          fontSize: "20px",
+                        }}>
+                        {card.rent}{" "}
+                        <span style={{ color: "black", fontSize: "12px" }}>
+                          Per Month
+                        </span>
+                      </Typography>
+                      <Typography sx={{ color: "#6F8CD3", fontSize: "20px" }}>
+                        <LocationOnIcon
+                          sx={{
+                            fontSize: "20px",
+                            mt: "-2px",
+                            color: "#6F8CD3",
+                          }}
+                        />
+                        {card.location}
+                        <span style={{ color: "black", fontSize: "12px" }}>
+                          km
+                        </span>
+                      </Typography>
+                    </Stack>
+                    {/* last update time */}
+                    <Box
+                      sx={{
+                        display: { xs: "none", md: "block" },
+                        alignItems: "center",
+                      }}>
+                      <Typography fontSize="12px" mt="8px" alignItems="center">
+                        <AccessTimeIcon
+                          sx={{ fontSize: "15px", mb: "-3px", mr: "2px" }}
+                        />
+                        {card.lastupdate}
+                      </Typography>
+                    </Box>
+                  </Stack>
+                  <Typography
+                    color="#6F8CD3"
+                    fontSize={{ xs: "15px", md: "20px" }}
+                    fontWeight={300}
+                    mt="4px">
+                    {card.title}
+                  </Typography>
+                  <Typography
+                    fontSize="14px"
+                    mt="0.5rem"
+                    color={"#808080"}
+                    sx={{ display: { xs: "none", sm: "block" } }}>
+                    {card.description}
+                  </Typography>
+                  <Stack direction="row" justifyContent="space-between">
+                    <Typography
+                      fontSize="15px"
+                      fontWeight={400}
+                      mt="8px"
+                      color={"#808080"}>
+                      {card.features}
+                    </Typography>
+
+                    <Typography
+                      width="20%"
+                      textAlign="center"
+                      underline="none"
+                      borderRadius="5px"
+                      py="12px"
+                      sx={{
+                        bgcolor: "#5CB85C",
+                        ":hover": { bgcolor: "#5CA05C" },
+                        color: "white",
+                        display: { xs: "none", md: "block" },
+                      }}>
+                      View Details
+                    </Typography>
+                  </Stack>
+                </Box>
+              </CardContent>
+            </CardActionArea>
+          </Link>
+        </Box>
+      ))}
+    </Box>
   );
 };
-// return (
-//   <Box>
-//     {data.map((card) => (
-//       <Box
-//         key={card.id}
-//         sx={{
-//           maxWidth: { xs: "100%", sm: "100%", md: "100%" },
-//           display: "flex",
-//           flexDirection: { xs: "column", sm: "row" },
-//           mb: 2,
-//           height: { md: 200 },
-//           backgroundColor: "#ffffff",
-//           boxShadow: "1px 1px 1px 1px #eaeaea",
-//         }}>
-//         <Carousel showIndicators={false} showThumbs={false}>
-//           {items.map((itemImg) => (
-//             // eslint-disable-next-line react/jsx-key
-//             <Box
-//               component="img"
-//               sx={{
-//                 height: 200,
-//                 display: "block",
-//                 maxWidth: { xs: "100%", sm: "400" },
-//                 objectFit: "cover",
-//                 overflow: "hidden",
-//                 width: "100%",
-//               }}
-//               src={itemImg.image}
-//               key={itemImg.id}
-//               alt={"haha"}
-//             />
-//           ))}
-//         </Carousel>
-//         <Link href="/DetailPage" underline="none" key={card.id}>
-//           <CardActionArea
-//             sx={{
-//               flexDirection: { xs: "column", sm: "row" },
-//             }}>
-//             <CardContent>
-//               <Box width={{ xs: "100%", sm: 500, md: "100%" }}>
-//                 <Stack direction="row" justifyContent="space-between">
-//                   {/* rent and location */}
-//                   <Stack
-//                     direction="row"
-//                     width={{ xs: "100%", md: "50%" }}
-//                     justifyContent="space-between"
-//                     alignItems="center">
-//                     <Typography
-//                       sx={{
-//                         color: "#6F8CD3",
-//                         fontSize: "20px",
-//                       }}>
-//                       {card.rent}{" "}
-//                       <span style={{ color: "black", fontSize: "12px" }}>
-//                         Per Month
-//                       </span>
-//                     </Typography>
-//                     <Typography sx={{ color: "#6F8CD3", fontSize: "20px" }}>
-//                       <LocationOnIcon
-//                         sx={{
-//                           fontSize: "20px",
-//                           mt: "-2px",
-//                           color: "#6F8CD3",
-//                         }}
-//                       />
-//                       {card.location}
-//                       <span style={{ color: "black", fontSize: "12px" }}>
-//                         km
-//                       </span>
-//                     </Typography>
-//                   </Stack>
-//                   {/* last update time */}
-//                   <Box
-//                     sx={{
-//                       display: { xs: "none", md: "block" },
-//                       alignItems: "center",
-//                     }}>
-//                     <Typography fontSize="12px" mt="8px" alignItems="center">
-//                       <AccessTimeIcon
-//                         sx={{ fontSize: "15px", mb: "-3px", mr: "2px" }}
-//                       />
-//                       {card.lastupdate}
-//                     </Typography>
-//                   </Box>
-//                 </Stack>
-//                 <Typography
-//                   color="#6F8CD3"
-//                   fontSize={{ xs: "15px", md: "20px" }}
-//                   fontWeight={300}
-//                   mt="4px">
-//                   {card.title}
-//                 </Typography>
-//                 <Typography
-//                   fontSize="14px"
-//                   mt="0.5rem"
-//                   color={"#808080"}
-//                   sx={{ display: { xs: "none", sm: "block" } }}>
-//                   {card.description}
-//                 </Typography>
-//                 <Stack direction="row" justifyContent="space-between">
-//                   <Typography
-//                     fontSize="15px"
-//                     fontWeight={400}
-//                     mt="8px"
-//                     color={"#808080"}>
-//                     {card.features}
-//                   </Typography>
-
-//                   <Typography
-//                     width="20%"
-//                     textAlign="center"
-//                     underline="none"
-//                     borderRadius="5px"
-//                     py="12px"
-//                     sx={{
-//                       bgcolor: "#5CB85C",
-//                       ":hover": { bgcolor: "#5CA05C" },
-//                       color: "white",
-//                       display: { xs: "none", md: "block" },
-//                     }}>
-//                     View Details
-//                   </Typography>
-//                 </Stack>
-//               </Box>
-//             </CardContent>
-//           </CardActionArea>
-//         </Link>
-//       </Box>
-//     ))}
-//   </Box>
-// );
-// };
 
 export default CardPage;
 
--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import "leaflet/dist/leaflet.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
 import createEmotionCache from "../utility/createEmotionCache";
-import lightTheme from "../styles/theme/lightTheme";
+import theme from "../components/Theme";
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -19,7 +19,7 @@ const MyApp = (props) => {
   return (
     <Box sx={{ backgroundColor: "#F5F5F5" }}>
       <CacheProvider value={emotionCache}>
-        <ThemeProvider theme={lightTheme}>
+        <ThemeProvider theme={theme}>
           <CssBaseline />
           <Navbar />
           <Component {...pageProps} />
